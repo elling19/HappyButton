d69d3bcfba5bef45d7787c3b28c2468105ad6c1b
elling <zhcshine@126.com>
2024-10-09 17:48:54 +0800

    fix(project): Bug fix.

--- Locales/Locale-zhCN.lua
+++ Locales/Locale-zhCN.lua
@@ -4,7 +4,7 @@ if not L then return end
 L["Welcome to use HappyToolkit."] = "欢迎使用插件HappyToolkit。"
 L["Can not register toolkit: must be a callback function."] = "无法注册：必须是回调函数。"
 L["You cannot use this in combat."] = "你无法在战斗中使用。"
-L["Teleprot"] = "传送"
+L["Teleport"] = "传送"
 L["Class"] = "职业"
 L["Profession"] = "专业"
 L["Mail"] = "邮箱"
