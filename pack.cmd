e207bac3b15e502bca37c171d084e0af130449c9
elling <zhcshine@126.com>
2024-10-28 17:49:28 +0800

    feat(project): Update pack cmd.

---
+++ pack.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set BASE_DIR=HappyButton-0.0.4
+
+set ZIPFILE=%BASE_DIR%.zip
+set TEMP_DIR=%BASE_DIR%
+
+mkdir "%TEMP_DIR%\HappyButton"
+
+:: use robocpy copy file exculde some dir
+robocopy . "%TEMP_DIR%\HappyButton" /E /XD ".git" ".idea" ".vscode" "%TEMP_DIR%" "HappyButton" /XF "pack.cmd"
+
+:: use 7zip 
+7z a -tzip "%ZIPFILE%" "%TEMP_DIR%\*"
+
+rd /S /Q "%TEMP_DIR%"
+
+if exist "%ZIPFILE%" (
+    echo success: %ZIPFILE%
+) else (
+    echo error
+)
+
+echo.
+echo type any to exist...
+pause >nul
+
+endlocal
