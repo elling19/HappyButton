497fb5e922482445cf08bb45accc39e627245e25
elling <zhcshine@126.com>
2024-11-06 17:59:05 +0800

    fix(project): Bug fix.

--- src/Core.lua
+++ src/Core.lua
@@ -27,7 +27,6 @@ function BarCore:Start()
     -- 注册相关事件以立即更新宏（如玩家登录或冷却更新）
     BarCore.Frame:RegisterEvent("ADDON_LOADED")             -- 插件加载
     BarCore.Frame:RegisterEvent("PLAYER_LOGIN")             -- 登录
-    BarCore.Frame:RegisterEvent("PLAYER_ENTERING_WORLD")    -- 玩家切换地图/重载界面
     BarCore.Frame:RegisterEvent("SPELL_UPDATE_COOLDOWN")    -- 触发冷却
     BarCore.Frame:RegisterEvent("PLAYER_REGEN_DISABLED")    -- 进入战斗事件
     BarCore.Frame:RegisterEvent("PLAYER_REGEN_ENABLED")     -- 退出战斗事件
@@ -39,10 +38,9 @@ function BarCore:Start()
     -- BarCore.Frame:RegisterEvent("UNIT_AURA")
     BarCore.Frame:SetScript("OnEvent", function(self, event, arg1)
         if event == "PLAYER_LOGIN" then
-            BarCore:Initial()
-        end
-        if event == "PLAYER_ENTERING_WORLD" then
-            HbFrame:ReloadAllEframeUI()
+            C_Timer.After(3, function()
+                BarCore:Initial()
+            end)
         end
         if event == "SPELL_UPDATE_COOLDOWN" or
             event == "PLAYER_EQUIPMENT_CHANGED" or
