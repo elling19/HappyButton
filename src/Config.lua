2eef7bebb7ae7b2d5bb6d6d0bfe94f12791be66a
elling <zhcshine@126.com>
2024-11-12 16:46:20 +0800

    feat(project): Update version.

--- src/Config.lua
+++ src/Config.lua
@@ -2256,7 +2256,7 @@ function ConfigOptions.Options()
                         order = 4,
                         width = 2,
                         type = "description",
-                        name = L["Version"] .. ": " .. "Beta-0.0.7"
+                        name = L["Version"] .. ": " .. "Beta-0.0.8"
                     }
                 }
             },
