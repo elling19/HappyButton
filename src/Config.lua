c34d4ca920628322638f67c77d12173e1dc41817
elling <elling19@outlook.com>
2026-02-01 14:37:15 +0800

    feat(project): 更新版本。

--- src/Config.lua
+++ src/Config.lua
@@ -2647,7 +2647,7 @@ function ConfigOptions.Options()
                         order = 4,
                         width = 2,
                         type = "description",
-                        name = L["Version"] .. ": " .. "Beta-0.2.14"
+                        name = L["Version"] .. ": " .. "Beta-0.2.15"
                     }
                 }
             },
